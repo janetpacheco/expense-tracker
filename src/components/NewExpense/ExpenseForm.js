a69f77777b918f45bedecc1b52f1d39a6f8c337c
janetpacheco <anne.janeta@gmail.com>
2022-10-11 12:36:22 -0600

    add two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,58 +2,92 @@ import React,{useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
+    // First approach
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredStore, setEnteredStore] =  useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');    
 
-    const[userInput,setUserInput] = useState({
-        enteredTitle:'',
-        enteredStore:'',
-        enteredSAmount:'',     
-        enteredSDate:'',
-    });
+    // const[userInput,setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredStore:'',
+    //     enteredSAmount:'',     
+    //     enteredSDate:'',
+    // });
     
 
     const titleChangeHandler = (event) =>{
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        });
+        setEnteredTitle(event.target.value);
+        
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+
+        // Asure the lastest state snapshot 
+        // setUserInput((prevState) => {
+        //    return {...prevState,enteredTitle:event.target.value}; 
+        // });
+
     };
 
     const storeChangeHandler = (event) =>{
-        setUserInput({
-            ...userInput,
-            enteredStore: event.target.value,
-        });
+        setEnteredStore(event.target.value);
+       
+        // setUserInput((prevState) => {
+        //     return {...prevState,enteredStore:event.target.value}; 
+        //  });
         
     };
 
     const amountChangeHandler = (event) =>{
-        setUserInput({
-            ...userInput,
-            enteredSAmount: event.target.value,
-        });
+        setEnteredAmount(event.target.value);
+        // setUserInput((prevState) => {
+        //     return {...prevState,enteredAmount:event.target.value}; 
+        //  });
         
     };
 
     const dateChangeHandler = (event) =>{
-        setUserInput({
-        ...userInput,
-        enteredSDate: event.target.value,
-        });
+        setEnteredDate(event.target.value);
+        // setUserInput((prevState) => {
+        //     return {...prevState,enteredDate:event.target.value}; 
+        //  });
+        
     };
 
+    const submitHandler =(event) =>{
+        event.preventDefault();
 
+        const expenseData = {
+            title: enteredTitle,
+            store: enteredStore,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredStore('walmart');
+
+    } ;
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
            <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                <input 
+                type='text' 
+                value={enteredTitle} 
+                onChange={titleChangeHandler}/>
             </div>
 
             <div className='new-expense__control'>
                 <label>Store</label>
-                <input type='text' onChange={storeChangeHandler}/>
+                <input 
+                type='text' 
+                value={enteredStore} 
+                onChange={storeChangeHandler}/>
             </div>
 
             <div className='new-expense__control'>
