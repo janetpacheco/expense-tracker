c8d55fce42c6003ce88389d57e83bb77deab0957
janetpacheco <anne.janeta@gmail.com>
2022-10-12 00:04:18 -0600

    add map and set a dynamic list

--- src/App.js
+++ src/App.js
@@ -1,52 +1,48 @@
 import React,{useState} from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-function App() { 
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Whole Wheat Bread',
-      amount: 1.67,
-      date: new Date(2022, 8, 24),
-      store: 'Walmart',
-    },
-    { 
-      id: 'e2', 
-      title: 'Winter Boots', 
-      amount: 125.98, 
-      date: new Date(2022, 8, 10),
-      store: 'Walmart', 
-    },
-      
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2022, 2, 25),
-      store: 'Walmart',
-    },
-    {
-      id: 'e4',
-      title: 'Sneakers',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      store: 'Walmart',
-    },
-  ];
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Whole Wheat Bread',
+    amount: 1.67,
+    date: new Date(2022, 8, 24),
+    store: 'Walmart',
+  },
+  { 
+    id: 'e2', 
+    title: 'Winter Boots', 
+    amount: 125.98, 
+    date: new Date(2022, 8, 10),
+    store: 'Walmart', 
+  },
+    
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2022, 2, 25),
+    store: 'Walmart',
+  },
+  {
+    id: 'e4',
+    title: 'Sneakers',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    store: 'Walmart',
+  },
+];
 
-  // const App = () =>{
-  //   const [expenses,setExpenses] = useState(DUMMY_DATA);
-  // };
 
-  // const addExpenseHandler = (expense) =>{    
-  //   setExpenses((previousExpenses) =>{
-  //     return [expense,...previousExpenses];
-  //   });
+const App = () => {
+
+  const [expenses,setExpenses] = useState(DUMMY_DATA); 
 
   const addExpenseHandler = (expense) =>{    
-      console.log('In App.js');
-      console.log(expense);
+    setExpenses((previousExpenses) =>{
+      return [expense,...previousExpenses]
+    });
   };
 
 
