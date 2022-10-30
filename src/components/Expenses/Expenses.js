3fb30d1a904d948e385e370109587ff4233f05dd
janetpacheco <99910759+AnaPachecoMind@users.noreply.github.com>
2022-10-30 16:38:21 -0600

    add conditional return statements refactor

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,6 @@ import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
-import ExpenseItem from './ExpenseItem';
 
 function Expenses(props) {
     
@@ -19,19 +18,7 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent =  <p>There are no expense records</p>;
-    if(filteredExpenses.length >0){
-       expensesContent =   filteredExpenses.length >0 &&
-       filteredExpenses.map((expense =>
-         <ExpenseItem 
-             key ={expense.id}
-             title = {expense.title}
-             store = {expense.store}
-             amount = {expense.amount}
-             date = {expense.date}
-         />
- ))
-    }
+   
     
     return (
     <div>
@@ -40,8 +27,7 @@ function Expenses(props) {
                 selected = {filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
-            {expensesContent}              
-            {/* <ExpensesList expenses = {filteredExpenses}/>            */}
+            <ExpensesList expenses = {filteredExpenses}/>           
         </Card>
     </div>
   );
